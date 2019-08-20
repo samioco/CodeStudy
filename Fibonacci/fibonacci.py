43a7ef07dac97f2c6bb02fa41fef724eb8db1e65
Sammy <samio1221@gmail.com>
2019-08-20 09:58:18 -0700

    Completed Fibonacci exercise

--- Fibonacci/fibonacci.py
+++ Fibonacci/fibonacci.py
@@ -7,7 +7,29 @@ def generate_fibonacci(n):
 		yield a
 		a,b = b,a+b
 
+def get_input():
+	while True:
+		try:
+			num_fibs = int(input("Enter number of Fibonacci calculations (Integer)> "))
+		except:
+			print("Input Error!  Please enter a valid integer!")
+			continue
+		else:
+			print(f"Thanks for inputting a valid integer: {num_fibs}")
+			break
+	return num_fibs
 
-for num in generate_fibonacci(10):
-	print(num)
-	
+def print_fibonacci(fib_series):
+	print(f"Generating Fibonacci's sequence to the {len(fib_series)}th position...")
+	print(fib_series)
+
+
+#MAIN
+num_fibs = get_input()
+
+fib_series = []
+for num in generate_fibonacci(num_fibs):
+	fib_series.append(num)
+
+print_fibonacci(fib_series)
+
