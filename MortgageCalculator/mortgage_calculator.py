4a75138179b7cd88a3357bfd42ed6caeba2c82d5
Sammy <samio1221@gmail.com>
2019-08-23 17:12:59 -0700

    Comment reminder: calculate_term math logic error reminder

--- MortgageCalculator/mortgage_calculator.py
+++ MortgageCalculator/mortgage_calculator.py
@@ -107,6 +107,8 @@ def calculate_payment():
 	print(f"Monthly payment: {monthly_payment}")
 	return monthly_payment
 
+#calculate_term formula not working
+#possible error in math logic
 def calculate_term():
 	p = float(principal)
 	r = float(interest_rate/100/12)
