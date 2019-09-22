67d140a2af4d1c7c9119ed4d973c191f753ca424
Sammy <samio1221@gmail.com>
2019-09-22 07:14:56 -0700

    FirstMissingPositive 2N solution

--- LeetCode_Problems/FirstMissingPositive.py
+++ LeetCode_Problems/FirstMissingPositive.py
@@ -1,32 +1,45 @@
 #2. Find smallest missing positive integer not in the list
 #[1,2,0] -> 3
 
-def firstMissingPositive(mylist):
+def firstMissingPositive_2N(mylist):
     mylist.sort()
     print("Received list: ",mylist)
-    print("Removing negatives...")
+    #print("Removing negatives, zero, and any number greater than length of list")
     for item in mylist:
-        if item<1:
+        if item<1 or item>len(mylist)+1:
             mylist.remove(item)
-            print("Removed: ",item,", mylist: ",mylist)
-    print("Cleaned list: ",mylist)
+            #print("Removed: ",item,", mylist: ",mylist)
+    #print("Cleaned list: ",mylist)
 
     i=1
     while len(mylist)>0:
-        print("Testing: ",i)
+        #print("Testing: ",i)
         if i in mylist:
-            print("Removing: ",i)
+            #print("Removing: ",i)
             mylist.remove(i)
-            print("Mylist: ",mylist)
+            #print("Mylist: ",mylist)
             i+=1
         else:
             print(i," not in list!")
             break
-    print("Returning: ",i)
+    #print("Returning: ",i)
     return i
 
-#list1 = [-5,1,2,0]
-#list1 = [3,4,-1,1]
+print("""
+First Missing Positive Algorithm:
+""")
+
+list1 = [-5,1,2,0]
+print("Input: ",list1)
+result = firstMissingPositive_2N(list1)
+print("Output: ",result,"\n")
+
+list1 = [3,4,-1,1]
+print("Input: ",list1)
+result = firstMissingPositive_2N(list1)
+print("Output: ",result,"\n")
+
 list1 = [7,8,9,11,12]
-result = firstMissingPositive(list1)
-print(result)
+print("Input: ",list1)
+result = firstMissingPositive_2N(list1)
+print("Output: ",result,"\n")
