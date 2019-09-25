8e668b7aaad3849a3a6e3d5ac7309f937572aa30
Sammy <samio1221@gmail.com>
2019-09-25 04:51:32 -0700

    cleanup of dict results logic and error handling

--- Python10RealWorldApps/EnglishDictionary/EnglishDictionary.py
+++ Python10RealWorldApps/EnglishDictionary/EnglishDictionary.py
@@ -12,21 +12,27 @@ def translate(w):
         result[w] = data[w]
         return result
     elif len(get_close_matches(w, data.keys())) > 0:
-        close_matches = get_close_matches(w, data.keys(), n=10)
+        close_matches = get_close_matches(w, data.keys(), n=5)
+        print("Close matches: ",close_matches)
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % close_matches[0])
-        if yn == "Y":
-            return data[close_matches[0]]
+        if yn.lower() == "y":
+            result[close_matches[0]] = data[close_matches[0]]
+            return result
         else:
-            return close_matches
+            print("\nReturning 5 closest matches...")
+            for match in close_matches:
+                result[match] = data[match]
+            return result
     else:
-        return "The word doesn't exist. Please double check it."
+        return "The word doesn't exist."
 
-word = input("Enter word: ")
+word = input("Enter word for lookup: ")
 output = translate(word)
 if type(output) == dict:
     for key in output.keys():
         for word_def in output[key]:
             print(key,": ", word_def)
+        print("")
 else:
     print(output)
 
