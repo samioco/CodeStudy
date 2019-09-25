490c6089c69f9fbfc9b6f950ea0c373de0f7dcd1
Sammy <samio1221@gmail.com>
2019-09-25 04:32:14 -0700

    EnglishDictionary: return dict of definitions

--- Python10RealWorldApps/EnglishDictionary/EnglishDictionary.py
+++ Python10RealWorldApps/EnglishDictionary/EnglishDictionary.py
@@ -4,26 +4,29 @@ from difflib import SequenceMatcher
 
 data = json.load(open("data.json"))
 
+#returns a dictionary of 'word':'definitions'
 def translate(w):
     w = w.lower()
+    result = {}
     if w in data:
-        return data[w]
+        result[w] = data[w]
+        return result
     elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        close_matches = get_close_matches(w, data.keys(), n=10)
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % close_matches[0])
         if yn == "Y":
-            return data[get_close_matches(w, data.keys())[0]]
-        elif yn == "N":
-            return "The word doesn't exist. Please double check it."
+            return data[close_matches[0]]
         else:
-            return "We didn't understand your entry."
+            return close_matches
     else:
         return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ")
 output = translate(word)
-if type(output) == list:
-    for item in output:
-        print(item)
+if type(output) == dict:
+    for key in output.keys():
+        for word_def in output[key]:
+            print(key,": ", word_def)
 else:
     print(output)
 
