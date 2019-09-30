831142749da56f505e228e22059f02efd8fc761f
Sammy <samio1221@gmail.com>
2019-09-29 18:49:45 -0700

    Webmaps03 Adding points using for loop

--- Python10RealWorldApps/Webmaps/map1.py
+++ Python10RealWorldApps/Webmaps/map1.py
@@ -4,7 +4,15 @@ Please use this instead:
 tiles = "Stamen Terrain"
 """
 import folium
+#Using FeatureGroup for Markers
 map = folium.Map(location=[38, -99], zoom_start=6, tiles = "Stamen Terrain")
-map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+map.add_child(fg)
 map.save("Map2.html")
+
+#Use a for loop to add list of coordinates
+for coordinates in [[38,-99], [39,-99]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Marker1", icon=folium.Icon(color='green')))
+map.add_child(fg)
+map.save("Map3.html")
