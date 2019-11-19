b83e4d601d00d09f5a97f96755dd3f3dad11598b
Sammy <samio1221@gmail.com>
2019-11-18 20:33:45 -0800

    js lbs to kg. add console.log()

--- FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/07-JavaScript_Level_One/js2.js
+++ FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/07-JavaScript_Level_One/js2.js
@@ -2,3 +2,6 @@ var weight_lb = prompt("Weight (lb):?")
 var weight_kg = weight_lb * 0.454
 
 alert("Weight (kg): " + weight_kg + " kilograms")
+console.log("Conversion Complete!")
+console.log("Converted from: " + weight_lb + "lbs")
+console.log("Converted to: " + weight_kg + "kg")
