d188e4045a5c88e1bbd852600bb9c57ee9de887f
Sammy <samio1221@gmail.com>
2019-11-21 14:00:50 -0800

    js more control flow if if else else examples

--- FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/07-JavaScript_Level_One/js3.js
+++ FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/07-JavaScript_Level_One/js3.js
@@ -1,5 +1,6 @@
 var hot = false
 var temp = 10
+console.log("Temp: "+temp)
 if (temp>80) {
   console.log("Hot Outside!");
 } else if (temp <= 80 && temp >= 50) {
@@ -9,3 +10,18 @@ if (temp>80) {
 } else {
   console.log("Freezing Outside!")
 }
+
+
+var ham = 10
+var cheese = 10
+console.log("")
+console.log("Ham: "+ham+", Cheese: "+cheese)
+if (ham>=10 && cheese>=10) {
+  report="Lots of ham and cheese sold!"
+} else if (ham===0 && cheese===0) {
+  report="Nothing sold!"
+} else {
+  report="Some sold!"
+}
+
+console.log(report)
