428c51e18ca9a85806b5de9ccb100bcb8c4c5cf8
Sammy <samio1221@gmail.com>
2019-11-30 13:49:47 -0800

    jquery 2: on() event input click

--- FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/10-jQuery/jquery2.js
+++ FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/10-jQuery/jquery2.js
@@ -1,13 +1,13 @@
-$('h1').click(function(){
-  console.log('There was a click!');
-  $(this).text("I was changed!")
-})
-
-
-$('li').click(function(){
-  console.log("an li item was clicked!");
-})
-
+// $('h1').click(function(){
+//   console.log('There was a click!');
+//   $(this).text("I was changed!")
+// })
+//
+//
+// $('li').click(function(){
+//   console.log("an li item was clicked!");
+// })
+//
 
 //key press
 // $('input').eq(0).keypress(function(event){
@@ -20,4 +20,23 @@ $('input').eq(0).keypress(function(event){
   if (event.which === 13) {
     $('h3').toggleClass('turnBlue');
   }
+})
+
+
+// on() method
+$('h1').on('dblclick',function(){
+  $(this).toggleClass('turnBlue');
+})
+
+$('li').on('mouseenter',function(){
+  $(this).toggleClass('turnRed');
+})
+
+
+// $('input').eq(1).on('click',function(){
+//   $('.container').fadeOut(3000)
+// })
+
+$('input').eq(1).on('click',function(){
+  $('.container').slideUp(3000)
 })
