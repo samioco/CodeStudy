1d8d56cb0031e2ec5fc9aaf6b00184fa3ff39e63
Sammy <samio1221@gmail.com>
2019-11-29 21:12:34 -0800

    jquery2: keypress event toggleClass

--- FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/10-jQuery/jquery2.js
+++ FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/10-jQuery/jquery2.js
@@ -1,73 +1,23 @@
-// jQuery makes it easy to interact with the DOM!
-
-// List of all possible events!
-// https://api.jquery.com/category/events/
-
-//////////////
-// CLICKS ///
-////////////
-
-// On Click
 $('h1').click(function(){
-  console.log("There was a click!");
+  console.log('There was a click!');
+  $(this).text("I was changed!")
 })
 
-// Click on multiple elements
-$('li').click(function() {
-  console.log("Click on any li !");
+
+$('li').click(function(){
+  console.log("an li item was clicked!");
 })
 
-// Using This with jQuery
-$('h3').click(function() {
-  $(this).text("I was changed!");
-})
 
-/////////////////
-// KEYPRESS ////
-///////////////
-// Using This with jQuery
-$('input').eq(0).keypress(function() {
-  $('h3').toggleClass("turnRed");
-})
+//key press
+// $('input').eq(0).keypress(function(event){
+//   $('h3').toggleClass('turnBlue');
+//   console.log(event);
+// })
 
-// We can use this event object, that has a ton of information!
-$('input').eq(0).keypress(function(event) {
-  console.log(event);
-})
 
-// Each Keyboard Key has a Keycode, for example Enter is 13
-$('input').eq(0).keypress(function(event) {
-  if(event.which === 13){
-    $('h3').toggleClass("turnRed");
+$('input').eq(0).keypress(function(event){
+  if (event.which === 13) {
+    $('h3').toggleClass('turnBlue');
   }
-})
-
-////////////
-// ON() ///
-//////////
-
-// on() basically works like addEventListener()
-$('h1').on("dblclick",function() {
-  $('h1').addClass('turnBlue');
-})
-
-$('li').on('mouseenter',function() {
-  $(this).toggleClass('turnRed');
-})
-
-/////////////////////////////
-// EFFECTS and ANIMATIONS //
-///////////////////////////
-
-// http://api.jquery.com/category/effects/
-
-$('input').eq(1).val("FADE OUT EVERYTHING");
-
-$('input').eq(1).on("click",function(){
-  $(".container").fadeOut(3000) ;
-})
-
-
-$('input').eq(1).on("click",function(){
-  $(".container").slideUp(1000) ;
 })
