d4bf524fdbc361b381544165fddc45d5b542ac64
Sammy <samio1221@gmail.com>
2019-12-16 02:40:38 -0800

    project two: templates and views

--- FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/14-Django_Level_One/ProTwo/ProTwo/urls.py
+++ FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/14-Django_Level_One/ProTwo/ProTwo/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url
+#from django.conf.urls import path
+from django.conf.urls import include
 from appTwo import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('help/', include('appTwo.urls')),
-    path('admin/', admin.site.urls),
+    url(r'^$', views.index, name="index"),
+    url(r'^help/', include('appTwo.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
