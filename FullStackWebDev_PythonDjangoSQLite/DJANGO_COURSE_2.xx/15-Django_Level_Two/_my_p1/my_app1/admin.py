874922a292922f75a1ed751ba536c288bbc523bc
Sammy <samio1221@gmail.com>
2019-12-21 09:37:40 -0800

    django2: review of django1

--- FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/15-Django_Level_Two/_my_p1/my_app1/admin.py
+++ FullStackWebDev_PythonDjangoSQLite/DJANGO_COURSE_2.xx/15-Django_Level_Two/_my_p1/my_app1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from my_app1.models import AccessRecord,Topic,Webpage
 
 # Register your models here.
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
